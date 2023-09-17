5048c6010e7c9be1811d91a767def7ef9d2e1c2e
linwrui <wenrui.lin@brilliant-solution.com>
2023-09-17 20:28:54 +0800

    Update 提取配色方案.js

--- 工作脚本/提取配色方案.js
+++ 工作脚本/提取配色方案.js
@@ -7,70 +7,58 @@
 // @require      https://cdn.bootcdn.net/ajax/libs/lodash.js/4.17.19/lodash.js
 // ==/UserScript==
 
-(async function () {
-  "use strict";
+;(async function () {
+  'use strict'
 
   // Your code here...
-  const layers = document.querySelector("#layers");
-  console.log(layers, layers.children);
-  let promise;
-  let count = 0;
-  let totalCount = 0;
+  const layers = document.querySelector('#layers')
+  console.log(layers, layers.children)
+  let promise
+  let count = 0
+  let totalCount = 0
   const children = _.sortBy(
     _.toArray(layers.children),
-    (o) => Number(o.style.top.replace("px", "")),
-    (o) => Number(o.style.left.replace("px", ""))
-  );
+    o => Number(o.style.top.replace('px', '')),
+    o => Number(o.style.left.replace('px', ''))
+  )
   for (let i = 0; i < children.length; i++) {
-    const el = children[i];
+    const el = children[i]
     if (promise) {
-      await promise;
+      await promise
     }
     if (!el.click) {
-      return;
+      return
     }
     if (count === 8) {
-      console.log("--------");
-      count = 0;
+      console.log('--------')
+      count = 0
     }
-    el.click();
-    promise = new Promise((resolve) =>
+    el.click()
+    promise = new Promise(resolve =>
       setTimeout(() => {
-        const colorFillSection = _.toArray(
-          document.querySelectorAll("section")
-        ).find((o) => o.firstChild.textContent === "填充");
+        const colorFillSection = _.toArray(document.querySelectorAll('section')).find(o => o.firstChild.textContent === '填充')
         const colorItem =
-          (colorFillSection &&
-            colorFillSection.querySelector(
-              '.item.items-group[data-label*="颜色"]'
-            )) ||
-          (colorFillSection &&
-            colorFillSection.querySelector(
-              '.item.items-group[data-label*="渐变"]'
-            ));
+          (colorFillSection && colorFillSection.querySelector('.item.items-group[data-label*="颜色"]')) ||
+          (colorFillSection && colorFillSection.querySelector('.item.items-group[data-label*="渐变"]'))
         if (colorItem) {
-          const isGradiant = colorItem
-            .getAttribute("data-label")
-            .includes("渐变");
+          const isGradiant = colorItem.getAttribute('data-label').includes('渐变')
           if (isGradiant) {
-            let colors = [];
-            const colorInputs = _.toArray(
-              colorItem.querySelectorAll("input[data-color]")
-            ).reverse();
-            console.log(JSON.stringify(colorInputs.map((o) => o.value)));
+            let colors = []
+            const colorInputs = _.toArray(colorItem.querySelectorAll('input[data-color]')).reverse()
+            console.log(JSON.stringify(colorInputs.map(o => o.value)))
           } else {
-            console.log(colorItem.querySelector("input[data-color]").value);
+            console.log(colorItem.querySelector('input[data-color]').value)
           }
-          count += 1;
-          totalCount += 1;
+          count += 1
+          totalCount += 1
         } else {
           // ignore
         }
-        resolve();
+        resolve()
       }, 80)
-    );
+    )
   }
   setTimeout(() => {
-    window.alert("done!" + totalCount);
-  }, 80);
-})();
+    window.alert('done!' + totalCount)
+  }, 80)
+})()
