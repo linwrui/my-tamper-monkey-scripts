d3bfd144cbe29d7279ce2aeff5e8ac401e4a4db9
linwrui <wenrui.lin@brilliant-solution.com>
2023-09-17 21:43:45 +0800

    Update 提取配色方案.js

--- 工作脚本/提取配色方案.js
+++ 工作脚本/提取配色方案.js
@@ -11,10 +11,74 @@
   'use strict'
   console.log('start!')
   // Your code here...
+  const colorPalette = {
+    default: {
+      label: '七彩缤纷',
+      colorList: [],
+      colorStops: [],
+    },
+    blueTech: {
+      label: '蓝色科技',
+      colorList: [],
+      colorStops: [],
+    },
+    blueBusiness: {
+      label: '蓝色商务',
+      colorList: [],
+      colorStops: [],
+    },
+    blueSimple: {
+      label: '蓝色简约',
+      colorList: [],
+      colorStops: [],
+    },
+    greenFresh: {
+      label: '绿色清新',
+      colorList: [],
+      colorStops: [],
+    },
+    greenSimple: {
+      label: '绿色简约',
+      colorList: [],
+      colorStops: [],
+    },
+    blueQuiet: {
+      label: '蓝色静谧',
+      colorList: [],
+      colorStops: [],
+    },
+    greenTech: {
+      label: '绿色科技',
+      colorList: [],
+      colorStops: [],
+    },
+    orangeBusiness: {
+      label: '橙色商务',
+      colorList: [],
+      colorStops: [],
+    },
+    blueFresh: {
+      label: '蓝色清新',
+      colorList: [],
+      colorStops: [],
+    },
+    greenBusiness: {
+      label: '绿色商务',
+      colorList: [],
+      colorStops: [],
+    },
+    orangeWarm: {
+      label: '橙色温暖',
+      colorList: [],
+      colorStops: [],
+    },
+  }
+  const colorPaletteList = Object.keys(colorPalette).map(key => colorPalette[key])
   const layers = document.querySelector('#layers')
   let promise
   let count = 0
   let totalCount = 0
+  let colorPaletteIndex = 0
   const children = _.sortBy(
     _.toArray(layers.children),
     o => Number(o.style.top.replace('px', '')),
@@ -31,6 +95,7 @@
     if (count === 8) {
       console.log('--------')
       count = 0
+      colorPaletteIndex += 1
     }
     el.click()
     promise = new Promise(resolve =>
@@ -40,12 +105,22 @@
           (colorFillSection && colorFillSection.querySelector('.item.items-group[data-label*="颜色"]')) ||
           (colorFillSection && colorFillSection.querySelector('.item.items-group[data-label*="渐变"]'))
         if (colorItem) {
-          const isGradiant = colorItem.getAttribute('data-label').includes('渐变')
-          if (isGradiant) {
-            let colors = []
-            const colorInputs = _.toArray(colorItem.querySelectorAll('input[data-color]')).reverse()
+          const isGradient = colorItem.getAttribute('data-label').includes('渐变')
+          if (isGradient) {
+            const colorInputs = _.toArray(colorItem.querySelectorAll('input[data-color]'))
+            colorPaletteList[colorPaletteIndex].colorList.push(colorInputs[0].value.split(' ')[0])
+            colorPaletteList[colorPaletteIndex].colorStops.push(
+              colorInputs.map((o, index) => {
+                const value = o.value.split(' ')
+                return {
+                  color: value[0],
+                  offset: index === 0 ? 0 : Number(((index + 1) / colorInputs.length).toFixed(1)),
+                }
+              })
+            )
             console.log(JSON.stringify(colorInputs.map(o => o.value)))
           } else {
+            colorPaletteList[colorPaletteIndex].colorList.push(colorItem.querySelector('input[data-color]').value.split(' ')[0])
             console.log(colorItem.querySelector('input[data-color]').value)
           }
           count += 1
@@ -57,7 +132,21 @@
       }, 80)
     )
   }
+
+  if (promise) {
+    await promise
+  }
+
+  console.log(colorPalette)
+
   setTimeout(() => {
-    window.alert('done!' + totalCount)
+    if (window.confirm('配色方案提取完成，已复制到剪贴板')) {
+      const textArea = document.createElement('textarea')
+      textArea.focus()
+      textArea.textContent = JSON.stringify(colorPalette)
+      document.body.appendChild(textArea)
+      textArea.select()
+      console.log(document.execCommand('copy'))
+    }
   }, 80)
 })()
